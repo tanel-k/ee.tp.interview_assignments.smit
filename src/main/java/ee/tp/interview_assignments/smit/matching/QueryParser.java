32c5d231cc5e4d9686584534acd14722228683a4
Tanel Prikk <tanel.prikk@outlook.com>
2019-06-25 00:34:02 +0300

    add unit tests for ee.tp.interview_assignments.smit.names.ClassName

--- src/main/java/ee/tp/interview_assignments/smit/matching/QueryParser.java
+++ src/main/java/ee/tp/interview_assignments/smit/matching/QueryParser.java
@@ -4,6 +4,8 @@ import ee.tp.interview_assignments.smit.matching.impl.CaseInsensitiveMatcher;
 import ee.tp.interview_assignments.smit.matching.impl.PrefixMatcher;
 import ee.tp.interview_assignments.smit.utils.StringUtils;
 
+import java.util.Objects;
+
 /**
  * Parser for class name query strings.
  *
@@ -13,6 +15,8 @@ public class QueryParser {
     private static final char EXACT_MATCH_MODIFIER = ' ';
 
     public static ClassNameMatcher parse(String queryString) {
+        Objects.requireNonNull(queryString);
+
         // Note: user is responsible for ensuring their query is "valid".
         // No reason to excessively restrict set of possible queries.
         boolean hasExactMatchModifier = queryString.charAt(queryString.length() - 1) == EXACT_MATCH_MODIFIER;
--- src/test/java/ee/tp/interview_assignments/smit/names/ClassNameTests.java
+++ src/test/java/ee/tp/interview_assignments/smit/names/ClassNameTests.java
@@ -1,4 +1,34 @@
 package ee.tp.interview_assignments.smit.names;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ClassNameTests {
+    @Test
+    public void testExtractSimpleNameFromUnqualifiedClass() {
+        assertEquals(
+            ClassName.of("Class").getSimpleName(),
+            "Class",
+            "Correctly extracts simple name from unqualified class name."
+        );
+    }
+
+    @Test
+    public void testExtractSimpleNameFromQualifiedClass() {
+        assertEquals(
+            ClassName.of("some.qualifier.path.Class").getSimpleName(),
+            "Class",
+            "Correctly extracts simple name from fully qualified class name."
+        );
+    }
+
+    @Test
+    public void testExtractSimpleNameFromSpecialClass() {
+        assertEquals(
+            ClassName.of("some.qualifier.path.$_class").getSimpleName(),
+            "$_class",
+            "Correctly extracts simple name from class name that uses special symbols."
+        );
+    }
 }
